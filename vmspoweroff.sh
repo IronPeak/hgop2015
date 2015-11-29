0da268aa4a930256c9f13260b17bf687ab0a85f2
IronPeak <hrafnorri1207@gmail.com>
2015-11-29 14:43:27 +0000

    VMsPoweroff SH - added an informative message to the user

--- vmspoweroff.sh
+++ vmspoweroff.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo 'Shutting down Virtual Machines'
 #poweroff all VMs, line from http://bealers.com/2014/07/halt-all-vagrant-virtualbox-vms-one-liner/
 for VM in `VBoxManage list runningvms | awk '{ print $2; }'`; do VBoxManage controlvm $VM poweroff; done
 
