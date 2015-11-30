2322175eba54a18c3b23387045c788ce52b96c67
IronPeak <hrafnorri1207@gmail.com>
2015-11-30 12:36:28 +0000

    Deploy SH - modified deploy script to for day 3

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-#poweroff all VMs
-./vmspoweroff.sh
+echo 'Pushing docker image'
 
 #Pushing image to docker
-(cd vagrant && 
-vagrant halt && 
+(cd vagrant &&  
 vagrant up && 
-vagrant ssh -c "(cd src/tictactoe/ && 
-		sudo service docker start && 
-		docker login --username=ironpeak && 
-		docker push ironpeak/tictactoe)" && 
+vagrant ssh -c '(cd src/tictactoe/
+		sudo service docker start 
+		docker push ironpeak/tictactoe
+		exit)' &&
 vagrant halt)
 
+echo 'Deploying docker image'
+
 #Deploying image to my production-test environment
-(cd .. && 
-cd HGOP2015 && 
-vagrant halt && 
+(cd vagrant/server/ && 
 vagrant up && 
-vagrant ssh -c "(docker login --username=ironpeak && 
-		docker pull ironpeak/tictactoe && 
-		docker run -p 8000:8080 -d -e NODE_ENV=production ironpeak/tictactoe)")
+vagrant ssh -c '(docker pull ironpeak/tictactoe
+		str=$(docker ps -f "name=productiontest" | grep "productiontest")
+		echo $str
+		if [ ! -z "$str" ]; then
+		    echo "attempting to remove"
+		    docker stop productiontest
+		    docker rm productiontest 
+		fi
+		docker run -p 8080:8080 -d -e NODE_ENV=production --name productiontest ironpeak/tictactoe
+		exit)')
 
 exit 0
