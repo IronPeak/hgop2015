a694494d88ea4ee247a7102214f5f4444cd9bb7a
ironpeak <hrafnorri1207@gmail.com>
2015-12-01 20:16:09 +0000

    Jenkins Test - changed jenkins script, testing wether jenkins will auto detect the change

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo 'Starting jenkins shell script'
+
 export PATH=/usr/local/bin:/path/to/node:/path/to/node_bin:/path/to/phantomjs:/path/to/jscoverage:$PATH;
 export DISPLAY=:0
 
@@ -7,5 +9,7 @@ npm install
 bower install
 
 ./dockerbuild.sh
+
+echo 'Finished jenkins shell script'
 
 exit 0
