500f183861846aa76135dc1b4d45d08e49d3a1f7
IronPeak <hrafnorri1207@gmail.com>
2015-12-07 11:48:57 +0000

    Jenkins Acceptance SH - fixed logic error that made the script always exit after deployment

--- jenkins-acceptance.sh
+++ jenkins-acceptance.sh
@@ -6,7 +6,7 @@ export ACCEPTANCE_URL=localhost:8080
 
 ./deploy.sh
 deployexitcode=$?
-if [ $deployexitcode = 0 ]; then
+if [ $deployexitcode != 0 ]; then
   echo "deploy exited with error code $deployexitcode"
   exit $deployexitcode
 fi
