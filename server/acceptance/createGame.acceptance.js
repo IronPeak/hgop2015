e4e2af654ce7e4ffcc7b25fd8ffae68526eb9670
IronPeak <hrafnorri1207@gmail.com>
2015-12-10 12:12:01 +0000

    Acceptance Tests - refactored some acceptance test to make them easier to read

--- server/acceptance/createGame.acceptance.js
+++ server/acceptance/createGame.acceptance.js
@@ -2,11 +2,11 @@
 
 var user = require('./user.acceptance');
 var given = require('./given.acceptance');
-var acceptanceUrl = process.env.ACCEPTANCE_URL;
 
 describe('CreateGame acceptance test:', function () {
 
     it('Should join game', function (done) {
-        given(user("HrafnOrri").createGame("CGTestGame1")).expect("GameCreated").isOk(done);
+        given(user("HrafnOrri").createGame("CGTestGame1"))
+	.expect("GameCreated").isOk(done);
     });
 });
--- server/acceptance/joinGame.acceptance.js
+++ server/acceptance/joinGame.acceptance.js
@@ -2,11 +2,12 @@
 
 var user = require('./user.acceptance');
 var given = require('./given.acceptance');
-var acceptanceUrl = process.env.ACCEPTANCE_URL;
 
 describe('JoinGame acceptance test:', function () {
 
     it('Should join game', function (done) {
-        given(user("HrafnOrri").createGame("JGTestGame1")).and(user("BaraDrofn").joinGame("JGTestGame1")).expect("GameJoined").isOk(done);
+        given(user("HrafnOrri").createGame("JGTestGame1"))
+	.and(user("BaraDrofn").joinGame("JGTestGame1"))
+	.expect("GameJoined").isOk(done);
     });
 });
