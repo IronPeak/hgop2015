7289ecb351d8894644b548e8a2f170ac7526780f
IronPeak <hrafnorri1207@gmail.com>
2015-12-11 10:46:59 +0000

    Acceptance Tests - added another acceptance test for MakeMove command

--- server/acceptance/makeMove.acceptance.js
+++ server/acceptance/makeMove.acceptance.js
@@ -11,4 +11,12 @@ describe('MakeMove acceptance test:', function () {
 	.and(user("HrafnOrri").makeMove(1, 1))
 	.expectEvent("MoveMade").byUser("HrafnOrri").atPosition(1, 1).isOk(done);
     });
+
+    it('player O can make a move', function (done) {
+        given(user("HrafnOrri").createGame("MMGTestGame2"))
+	.and(user("BaraDrofn").joinGame("MMGTestGame2"))
+	.and(user("HrafnOrri").makeMove(1, 1))
+	.and(user("BaraDrofn").makeMove(2, 1))
+	.expectEvent("MoveMade").byUser("BaraDrofn").atPosition(2, 1).isOk(done);
+    });
 });
