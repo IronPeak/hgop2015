21b7c440efb7feebfe2733184538fa6a268fc7c5
IronPeak <hrafnorri1207@gmail.com>
2015-12-12 12:41:10 +0000

    Pipeline Debug - confirming that changes make it into production

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -3,14 +3,6 @@
 echo Cleaning...
 rm -rf ./dist
 
-if [ -z "$GIT_COMMIT" ]; then
-  export GIT_COMMIT=$(git rev-parse HEAD)
-  export GIT_URL=$(git config --get remote.origin.url)
-fi
-
-# Remove .git from url in order to get https link to repo (assumes https url for GitHub)
-export GITHUB_URL=$(echo $GIT_URL | rev | cut -c 5- | rev)
-
 echo Building app
 grunt
 gruntexitcode=$?
@@ -19,23 +11,11 @@ if [ $gruntexitcode != 0 ]; then
     exit $gruntexitcode
 fi
 
-cat > ./dist/githash.txt <<_EOF_
-$GIT_COMMIT
-_EOF_
-
-cat > ./dist/public/version.html << _EOF_
-<!doctype html>
-<head>
-   <title>TicTacToe version information</title>
-</head>
-<body>
-   <span>Origin:</span> <span>$GITHUB_URL</span>
-   <span>Revision:</span> <span>$GIT_COMMIT</span>
-   <p>
-   <div><a href="$GITHUB_URL/commits/$GIT_COMMIT">History of current version</a></div>
-</body>
-_EOF_
-
+rc=$?
+if [[ $rc != 0 ]] ; then
+    echo "Grunt build failed with exit code " $rc
+    exit $rc
+fi
 
 cp ./Dockerfile ./dist/
 
@@ -47,22 +27,29 @@ if [ $npmexitcode != 0 ]; then
     exit $npmexitcode
 fi
 
-echo "Building docker image"
+echo Building docker image
 sudo service docker start
-docker build -t ironpeak/tictactoe:$GIT_COMMIT .
+docker build -t ironpeak/tictactoe .
 buildexitcode=$?
 if [ $buildexitcode != 0 ]; then
     echo "docker build exited with error code $buildexitcode"
     exit $buildexitcode
 fi
 
-docker push ironpeak/tictactoe:$GIT_COMMIT
+docker push ironpeak/tictactoe
+pushexitcode=$?
+if [ $pushexitcode != 0 ]; then
+    echo "docker push exited with error code $pushexitcode"
+    exit $pushexitcode
+fi
+
 rc=$?
 if [[ $rc != 0 ]] ; then
-    echo "Docker push failed " $rc
+    echo "Docker build failed " $rc
     exit $rc
 fi
 
 echo "Done"
 
 exit 0
+
