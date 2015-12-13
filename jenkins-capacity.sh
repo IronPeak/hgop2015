2f61131d051099eb1b292a7c793482e2d8f5b220
IronPeak <hrafnorri1207@gmail.com>
2015-12-13 13:48:31 +0000

    Jenkins Capacity SH - updated jenkins capacity script to install npm and bower packages

--- jenkins-capacity.sh
+++ jenkins-capacity.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+export DISPLAY=:0
+export PATH=/usr/local/bin:$PATH;
 export ACCEPTANCE_URL=192.168.50.4:8080
+
+npm install
+npmexitcode=$?
+if [ $npmexitcode != 0 ]; then
+  echo "npm install exited with error code $npmexitcode"
+  exit $npmexitcode
+fi
+
+bower install
+bowerexitcode=$?
+if [ $bowerexitcode != 0 ]; then
+  echo "bower install exited with error code $bowerexitcode"
+  exit $bowerexitcode
+fi
 
 grunt mochaTest:load
 capacityexitcode=$?
