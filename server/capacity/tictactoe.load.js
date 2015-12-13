a924c741d6da7a4fd4f45224ef137d5d9b88cd29
IronPeak <hrafnorri1207@gmail.com>
2015-12-13 15:49:21 +0000

    Capacity Test - capacity test fails if it exceeds the timeout

--- server/capacity/tictactoe.load.js
+++ server/capacity/tictactoe.load.js
@@ -3,23 +3,28 @@ var given = require('../fluid-api/tictactoeFluid').given;
 
 describe('Capacity Tests:', function() {
 
-    var gamesToPlay = 1000;
-    var x = 1;
+    it('Should play 1000 games in 5 seconds.', function(done) {
 
-    this.timeout(x * 1000);
-
-    it('Should play ' + gamesToPlay + ' games in ' + x + ' seconds.', function(done) {
+	var startTime = new Date().getTime();
+        var gamesToPlay = 1000;
+        var x = 6;
 
         var doneCount = 0;
 
         var QED = function() {
             if (gamesToPlay === ++doneCount) {
-                done();
+		var currentTime = new Date().getTime();
+                var elapsedTime = currentTime - startTime;
+		if(x * 1000 < elapsedTime) {
+		    done(new Error(elapsedTime + " should be below " + (x * 1000)));		
+		} else {
+                    done();
+		}
             }
         };
 
         for (var gid = 0; gid < gamesToPlay; gid++) {
-            /*given(user("TestUser1").createGame("CapacityTest" + gid).named("Game" + gid))
+            given(user("TestUser1").createGame("CapacityTest" + gid).named("Game" + gid))
                 .and(user("TestUser2").joinGame("CapacityTest" + gid))
                 .and(user("TestUser1").as('X').makeMove(0, 0))
                 .and(user("TestUser2").as('O').makeMove(0, 1))
@@ -30,8 +35,7 @@ describe('Capacity Tests:', function() {
                 .and(user("TestUser1").as('X').makeMove(2, 1))
                 .and(user("TestUser2").as('O').makeMove(2, 0))
                 .and(user("TestUser1").as('X').makeMove(2, 2))
-                .expectEvent("GameDraw").byUser("TestUser1").isOk(QED);*/
-	    setTimeout(done, 100);
+                .expectEvent("GameDraw").byUser("TestUser1").isOk(QED);
         }
     });
 });
