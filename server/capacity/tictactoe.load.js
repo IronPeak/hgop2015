f2114b3cfe095d5074419481b7356cd7cf8dbd69
IronPeak <hrafnorri1207@gmail.com>
2015-12-13 14:01:51 +0000

    Capacity Test - lowering the limit of the capacity test to find out where it fails

--- server/capacity/tictactoe.load.js
+++ server/capacity/tictactoe.load.js
@@ -1,10 +1,10 @@
 var user = require('../fluid-api/tictactoeFluid').user;
 var given = require('../fluid-api/tictactoeFluid').given;
 
-it('Should play 1000 games in 5 seconds.', function(done) {
+it('Should play 1000 games in 3 seconds.', function(done) {
     var doneCount = 0;
     var gamesToPlay = 1000;
-    var x = 5;
+    var x = 3;
 
     this.timeout(x * 1000);
 
