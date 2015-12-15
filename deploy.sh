c6a577d9bf17bacc34130fd94dacef7e0e902b34
ironpeak <hrafnorri1207@gmail.com>
2015-12-15 16:54:18 +0000

    Deploy SH - removed a variable from deployment script that was not working due to qoutes

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,7 @@ if [ $pulldocker != 0 ]; then
 fi
 
 echo -e '\nKilling and removing current'
-ssh vagrant@192.168.50.4 "(str=$(docker ps -a -f name=production$2 | grep production$2)
-                          if [ ! -z "$str" ]; then
+ssh vagrant@192.168.50.4 "(if [ ! -z $(docker ps -a -f name=production$2 | grep production$2) ]; then
                             echo -e '\nKilling current'
                             docker kill production$2
                             echo -e '\nRemoving old'
