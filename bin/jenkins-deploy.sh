5a7b213c097f95afccd9b6a0fabbc587c84b6d41
ironpeak <hrafnorri1207@gmail.com>
2015-12-17 10:32:50 +0000

    Jenkins Deploy SH - added output to script

--- bin/jenkins-deploy.sh
+++ bin/jenkins-deploy.sh
@@ -2,11 +2,15 @@
 
 GIT_UPSTREAM_HASH=$(<dist/githash.txt)
 
+echo "Starting jenkins deploy script"
+
 ./bin/dockerdeploy.sh $GIT_UPSTREAM_HASH 9090
 deployexitcode=$?
 if [ $deployexitcode != 0 ]; then
   echo "deploy exited with error code $deployexitcode"
   exit $deployexitcode
 fi
+
+echo "jenkins deploy script finished"
 
 exit 0
