eeaaf640447eac141c5d82598baa3688535dec97
ironpeak <hrafnorri1207@gmail.com>
2015-12-18 11:53:43 +0000

    Docker Build SH - trying to fix the GITHUB URL variable in docker build script

--- bin/dockerbuild.sh
+++ bin/dockerbuild.sh
@@ -9,8 +9,7 @@ if [ -z "$GIT_COMMIT" ]; then
   export GIT_URL=$(git config --get remote.origin.url)
 fi
 
-# Remove .git from url in order to get https link to repo (assumes https url for GitHub)
-export GITHUB_URL=$(echo $GIT_URL | rev | cut -c 5- | rev)
+export GITHUB_URL=$(echo $GIT_URL)
 
 echo Building app
 grunt
