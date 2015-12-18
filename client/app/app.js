6e5a01c54781fb1d21b0b8d2edc67b94acbbb890
ironpeak <hrafnorri1207@gmail.com>
2015-12-18 14:38:11 +0000

    Refactoring Code - indenting code

--- client/app/app.js
+++ client/app/app.js
@@ -1,28 +1,29 @@
 'use strict';
 
 angular.module('tictactoeApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ui.router',
-  'ui.bootstrap'
-])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ui.router',
+        'ui.bootstrap'
+    ])
+    .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
 
-    $urlRouterProvider
-      .otherwise('/');
+        $urlRouterProvider
+            .otherwise('/');
 
-    $locationProvider.html5Mode(true);
-  }).value('guid', function() {
-    function s4() {
-      return Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16)
-        .substring(1);
-    }
-    function calculate() {
-      return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-        s4() + '-' + s4() + s4() + s4();
-    }
-    return calculate();
-  });
+        $locationProvider.html5Mode(true);
+    }).value('guid', function() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+
+        function calculate() {
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                s4() + '-' + s4() + s4() + s4();
+        }
+        return calculate();
+    });
