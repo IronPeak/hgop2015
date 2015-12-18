e69c33c7a86793f9fdbbd76dc14b743a7e2729de
ironpeak <hrafnorri1207@gmail.com>
2015-12-18 14:43:55 +0000

    Refactoring Client components

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,15 +1,15 @@
 'use strict';
 
 angular.module('tictactoeApp')
-  .controller('NavbarCtrl', function ($scope, $location) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
+    .controller('NavbarCtrl', function($scope, $location) {
+        $scope.menu = [{
+            'title': 'Home',
+            'link': '/'
+        }];
 
-    $scope.isCollapsed = true;
+        $scope.isCollapsed = true;
 
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
-  });
+        $scope.isActive = function(route) {
+            return route === $location.path();
+        };
+    });
